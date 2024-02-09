b2b5d0bb1c3996b38640ef053e52d8d60f96d0b3
Muhammad Adnan <61709395+Adnan-Dani@users.noreply.github.com>
2024-02-09 13:10:08 +0500

    issu details page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,6 +1,9 @@
 import prisma from "@/prisma/client";
+import { Flex, Heading, Text, Card } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import React from "react";
+import IssueStatusBadge from "../../components/IssueStatusBadge";
+import Markdown from "react-markdown";
 
 interface Props {
   params: { id: string };
@@ -14,11 +17,14 @@ const IssueDetailPage = async ({ params }: Props) => {
   if (!issue) return notFound();
   return (
     <div>
-      Issue Details
-      <p>{issue.title}</p>
-      <p>{issue.description}</p>
-      <p>{issue.status}</p>
-      <p>{issue.createdAt.toDateString()}</p>
+      <Heading>{issue.title}</Heading>
+      <Flex my="2" className="space-x-3">
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card className="prose ">
+        <Markdown>{issue.description}</Markdown>
+      </Card>
     </div>
   );
 };
