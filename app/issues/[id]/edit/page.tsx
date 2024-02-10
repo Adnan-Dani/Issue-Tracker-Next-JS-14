8341ad9a8e6a07614d63d43527e38a9b2095d810
Muhammad Adnan <61709395+Adnan-Dani@users.noreply.github.com>
2024-02-10 11:12:06 +0500

    build the edit issue page

---
+++ app/issues/[id]/edit/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import prisma from "@/prisma/client";
+import { notFound } from "next/navigation";
+import IssueForm from "../../_components/IssueForm";
+
+interface Props {
+  params: { id: string };
+}
+
+const EditIssuePage = async ({ params }: Props) => {
+  const issue = await prisma?.issue?.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!issue) return notFound();
+  return (
+    <>
+      <IssueForm issue={issue} />
+    </>
+  );
+};
+
+export default EditIssuePage;
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -2,6 +2,7 @@ import { IssueStatusBadge, Link } from "@/app/components";
 import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
 import IssueAction from "./issueAction";
+import { Issue } from "@prisma/client";
 
 const IssuesPage = async () => {
   const issues = await prisma?.issue.findMany();
@@ -22,7 +23,7 @@ const IssuesPage = async () => {
         </Table.Header>
 
         <Table.Body>
-          {issues.map((issue) => (
+          {issues.map((issue: Issue) => (
             <Table.Row key={issue.id}>
               <Table.RowHeaderCell>
                 <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
