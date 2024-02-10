07b2bc74675415ba189a4e53d311f756e2a00210
Muhammad Adnan <61709395+Adnan-Dani@users.noreply.github.com>
2024-02-10 11:22:14 +0500

    edit page bug fix

--- app/issues/[id]/edit/page.tsx
+++ app/issues/[id]/edit/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import prisma from "@/prisma/client";
 import { notFound } from "next/navigation";
 import IssueForm from "../../_components/IssueForm";
@@ -8,8 +6,9 @@ interface Props {
   params: { id: string };
 }
 
+// eslint-disable-next-line @next/next/no-async-client-component
 const EditIssuePage = async ({ params }: Props) => {
-  const issue = await prisma?.issue?.findUnique({
+  const issue = await prisma.issue?.findUnique({
     where: { id: parseInt(params.id) },
   });
   if (!issue) return notFound();
