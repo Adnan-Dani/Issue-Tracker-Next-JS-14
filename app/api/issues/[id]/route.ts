271ace0b877256e72da10b8644d357aa1d3a084a
Muhammad Adnan <61709395+Adnan-Dani@users.noreply.github.com>
2024-02-16 22:36:58 +0500

    Delete issue api

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -2,6 +2,23 @@ import { issueSchema } from "@/app/validationSchema";
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
 
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!issue)
+    return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
+
+  await prisma.issue.delete({
+    where: { id: parseInt(params.id) }
+  })
+  return NextResponse.json({ error: "Successfully Deleted." });
+
+}
+
 export async function PATCH(
   request: NextRequest,
   { params }: { params: { id: string } }
