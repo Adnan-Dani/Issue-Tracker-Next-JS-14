89f0d5afbf99dd4dfc4305fdacb21e0fcc9bb50a
Muhammad Adnan <61709395+Adnan-Dani@users.noreply.github.com>
2024-02-18 21:58:18 +0500

    secure the application

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,11 +1,16 @@
 import { issueSchema } from "@/app/validationSchema";
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/auth/authOptions";
 
 export async function DELETE(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
+  const session = await getServerSession(authOptions);
+  if (!session) return NextResponse.json("Access Denied", { status: 403 });
+
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
@@ -13,16 +18,17 @@ export async function DELETE(
     return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
 
   await prisma.issue.delete({
-    where: { id: parseInt(params.id) }
-  })
+    where: { id: parseInt(params.id) },
+  });
   return NextResponse.json({ error: "Successfully Deleted." });
-
 }
 
 export async function PATCH(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
+  const session = await getServerSession(authOptions);
+  if (!session) return NextResponse.json("Access Denied", { status: 403 });
   const body = await request.json();
   const validation = issueSchema.safeParse(body);
   if (!validation.success)
