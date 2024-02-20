b2da0967b73e545ef31d3382fb212ec957f4088d
Muhammad Adnan <61709395+Adnan-Dani@users.noreply.github.com>
2024-02-20 21:02:36 +0500

    users api integrate in assignes

--- app/issues/[id]/AssigneList.tsx
+++ app/issues/[id]/AssigneList.tsx
@@ -1,15 +1,31 @@
 "use client";
 
+import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const AssigneList = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  useEffect(() => {
+    const fetchUser = async () => {
+      const { data } = await axios.get<User[]>("/api/users");
+      console.log(data);
+      setUsers(data);
+    };
+    fetchUser();
+  }, []);
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assigne..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="1">Adnan</Select.Item>
+          {users.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
